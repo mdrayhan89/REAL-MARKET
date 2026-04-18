2f7be3f2201186086b8e578668bb7f17acd87f19
mdrayhan89 <islamrabby655@gmail.com>
2026-04-18 23:24:18 +0600

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+# ১. লাইব্রেরি ইন্সটল করা
 pip install -r requirements.txt
-playwright install chromium
+
+# ২. Playwright এবং এর ব্রাউজার সঠিকভাবে ইন্সটল করা
+python -m playwright install chromium
